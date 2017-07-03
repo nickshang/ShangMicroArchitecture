01f972263173a6728aadf7ae53b05660c24101a7
ssj00@163.com <ssj00@163.com>
2017-07-03 16:12:54 +0800

    添加新配置类

---
+++ src/main/java/com/shang/springboot/readinglist/config/DruidConfig.java
@@ -0,0 +1,83 @@
+package com.shang.springboot.readinglist.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Druid配置
+ * Created by Think on 2017/6/30.
+ */
+@Configuration
+@Slf4j
+public class DruidConfig {
+
+    private  DataSourceConfig dataSourceConfig;
+
+    @Autowired
+    public DruidConfig(DataSourceConfig  dataSourceConfig){
+        this.dataSourceConfig = dataSourceConfig;
+    }
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        reg.addInitParameter("loginUsername", "druid");
+        reg.addInitParameter("loginPassword", "druid");
+        return reg;
+    }
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+        return filterRegistrationBean;
+    }
+
+    @Bean(name = "druidDataSource", initMethod = "init", destroyMethod = "close")
+    @Primary
+    public DataSource druidDataSource(){
+        DruidDataSource datasource = new DruidDataSource();
+
+        datasource.setUrl(this.dataSourceConfig.getUrl());
+        datasource.setUsername(this.dataSourceConfig.getUsername());
+        datasource.setPassword(this.dataSourceConfig.getPassword());
+        datasource.setDriverClassName(this.dataSourceConfig.getDriverClassName());
+
+        datasource.setInitialSize(this.dataSourceConfig.getInitialSize());
+        datasource.setMinIdle(this.dataSourceConfig.getMinIdle());
+        datasource.setMaxActive(this.dataSourceConfig.getMaxActive());
+        datasource.setMaxWait(this.dataSourceConfig.getMaxWait());
+        datasource.setTimeBetweenEvictionRunsMillis(this.dataSourceConfig.getTimeBetweenEvictionRunsMillis());
+        datasource.setMinEvictableIdleTimeMillis(this.dataSourceConfig.getMinEvictableIdleTimeMillis());
+        datasource.setValidationQuery(this.dataSourceConfig.getValidationQuery());
+        datasource.setTestWhileIdle(this.dataSourceConfig.isTestWhileIdle());
+        datasource.setTestOnBorrow(this.dataSourceConfig.isTestOnBorrow());
+        datasource.setTestOnReturn(this.dataSourceConfig.isTestOnReturn());
+        datasource.setPoolPreparedStatements(this.dataSourceConfig.isPoolPreparedStatements());
+        try {
+            datasource.setFilters(this.dataSourceConfig.getFilters());
+        } catch (SQLException e) {
+            log.error("druid configuration initialization filter", e);
+        }
+        return datasource;
+    }
+
+}
---
+++ src/main/java/com/shang/springboot/readinglist/config/SessionFactoryConfig.java
@@ -0,0 +1,75 @@
+package com.shang.springboot.readinglist.config;
+
+/**
+ * Created by Think on 2017/6/30.
+ */
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.shang.springboot.readinglist.mapper")
+public class SessionFactoryConfig implements TransactionManagementConfigurer{
+
+    /** * mybatis 配置路径 */
+    private static String MYBATIS_CONFIG = "mybatis-config.xml";
+
+
+    private DataSource dataSource;
+//
+//    private String typeAliasPackage = "com.xxx.xxx.mybati.model";
+
+    @Autowired
+    public SessionFactoryConfig(@Qualifier("druidDataSource")DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    /**
+     *创建sqlSessionFactoryBean 实例
+     * 并且设置configtion 如驼峰命名.等等
+     * 设置mapper 映射路径
+     * 设置datasource数据源
+     * @return
+     * @throws Exception
+     */
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactoryBean createSqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        /** 设置mybatis configuration 扫描路径 */
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(MYBATIS_CONFIG));
+        /** 设置datasource */
+        sqlSessionFactoryBean.setDataSource(dataSource);
+//        /** 设置typeAlias 包扫描路径 */
+//        sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasPackage);
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:com/shang/springboot/sqlmap/readlinglist/*.xml"));
+
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
